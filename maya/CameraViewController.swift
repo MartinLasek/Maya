dd0525ccc0d9d67123bc637d1654682baa16327a
Martin Lasek <martinlasek@hotmail.de>
2017-02-18 22:37:22 +0100

    increased camera appearance alot

--- maya/CameraViewController.swift
+++ maya/CameraViewController.swift
@@ -22,7 +22,9 @@ class CameraViewController: UIViewController {
     do {
       let cameraView = try cameraDispatcher.getFrontCameraView(view: self.view)
       cameraView.layer.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-      self.view.addSubview(cameraView)
+      
+      // used transition here because it is way faster than 'self.view.addSubview(cameraView)'
+      UIView.transition(from: self.view, to: cameraView, duration: 0, options: UIViewAnimationOptions.transitionCurlUp, completion: nil)
     } catch {
       return
     }
