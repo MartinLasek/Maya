512982d152c7675db5b1fed64b539c4a309ec2df
Martin Lasek <martinlasek@hotmail.de>
2017-02-20 00:01:46 +0100

    refactored code for positioning next/close button

--- maya/TakenPhotoView.swift
+++ maya/TakenPhotoView.swift
@@ -27,9 +27,30 @@ class TakenPhotoView {
     self.view.frame = bounds
     
     nextButton.setImage(nextImage, for: .normal)
-    nextButton.layer.frame = CGRect(x: (self.bounds.size.width / 6) * 5 - 25, y: self.bounds.size.height - 125, width: CGFloat(50), height: CGFloat(50))
+    nextButton.layer.frame = positionNextButton()
     
     closeButton.setImage(closeImage, for: .normal)
-    closeButton.layer.frame = CGRect(x: (self.bounds.size.width / 6) - 20, y: self.bounds.size.height - 120, width: CGFloat(40), height: CGFloat(40))
+    closeButton.layer.frame = positionCloseButton()
+  }
+  
+  func positionNextButton() -> CGRect {
+    let buttonSize: CGFloat = 50
+    // the tab bar is having 3 fields in which each field an icon is at the center
+    // view width divided by 3 and again by 2 is the center of the first field,
+    // dividing the item width by half will position the item visually at the center
+    // if you'd multiply field positon by 6 again, you are at the screen edge right side
+    let x = (self.bounds.size.width / 6) * 5 - (buttonSize / 2)
+    let y = self.bounds.size.height - 125
+    
+    return CGRect(x: x, y: y, width: buttonSize, height: buttonSize)
+  }
+  
+  func positionCloseButton() -> CGRect {
+    let buttonSize: CGFloat = 40
+    // see 'positionNextButton()' comment for mathmatical explanation
+    let x = (self.bounds.size.width / 6) - buttonSize / 2
+    let y = self.bounds.size.height - 120
+    
+    return CGRect(x: x, y: y, width: buttonSize, height: buttonSize)
   }
 }
